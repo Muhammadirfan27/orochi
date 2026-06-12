83a4667557b4ca74c7d2f5c3a35b275190e95163
Muhammadirfan27 <81164197+Muhammadirfan27@users.noreply.github.com>
2026-06-12 17:05:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ try:
     genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
     # Menggunakan gemini-1.5-pro yang lebih stabil
   
-    model = genai.GenerativeModel('gemini-1.0-pro')
+    model = genai.GenerativeModel('gemini-1.5-flash-latest')
 except Exception as e:
     st.error(f"Error konfigurasi API: {e}")
 
